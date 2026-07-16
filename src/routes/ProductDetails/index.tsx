1207d0d942f653890e2d124d82fd4bf2fdc275c3
Vinicius Fernandes <contato.viniciusfernandes9@gmail.com>
2026-07-16 12:14:25 -0300

    test Props

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -3,6 +3,29 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import type { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Esta TV é foda!",
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
+  price: 3690.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos"
+    },
+    {
+      id: 3,
+      name: "Computadores"
+    },
+    {
+      id: 4,
+      name: "Importados"
+    }
+  ]
+}
 
 export default function ProductDetails() {
 
@@ -11,7 +34,7 @@ export default function ProductDetails() {
       <HeaderClient />
       <main>
         <section id="product-details-section" className="nxf-container">
-          <ProductDetailsCard />
+          <ProductDetailsCard product={product} />
           <div className="nxf-btn-page-container">
             <ButtonPrimary />
             <ButtonInverse />
